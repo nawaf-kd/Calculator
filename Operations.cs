050394d024776d2e31f04200ae247874ced898c0
nawaf-kd <74269604+nawaf-kd@users.noreply.github.com>
2023-05-25 01:41:20 +0300

    Add files via upload

---
+++ Operations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Operations
+    {
+        public Double Add(Double value1,Double value2)
+        {
+            return value1 + value2;
+        }
+
+        public Double Sub(Double value1, Double value2)
+        {
+            return value1 - value2;
+        }
+
+        public Double Mul(Double value1, Double value2)
+        {
+            return value1 * value2;
+        }
+
+        public Double Div(Double value1, Double value2)
+        {
+            if (value2 == 0)
+            {
+                Console.WriteLine("Can't divide by 0");
+            }
+              return value1 / value2;
+            
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Operations operations = new Operations();
+
+            String exit;
+
+            do
+            {
+
+                Console.WriteLine("Enter your first number:");
+                Double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter your Second number:");
+                Double num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter operation +, -, *, /. ?");
+                char sign = Convert.ToChar(Console.ReadLine());
+
+                switch (sign)
+                {
+                    case '+':
+                        Console.WriteLine("Result = " + operations.Add(num1, num2));
+                        break;
+                    case '-':
+                        Console.WriteLine("Result = " + operations.Sub(num1, num2));
+                        break;
+                    case '*':
+                        Console.WriteLine("Result = " + operations.Mul(num1, num2));
+                        break;
+                    case '/':
+                        Console.WriteLine("Result = " + operations.Div(num1, num2));
+                        break;
+                        default: Console.WriteLine("Please Enter an operation");
+                        break;
+
+                        
+
+                }
+                Console.WriteLine("to exit enter y  ");
+                exit = Convert.ToString(Console.ReadLine());
+            } while (exit != "y");
+
+    {
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
